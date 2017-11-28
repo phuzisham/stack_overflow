aa8f9ef292faa302025f8d926a9e992c0e3344c9
David Hale <david.hale7@gmail.com>
2017-11-28 15:29:20 -0800

    add sign in with bCrypt

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   #   resources :reviews
   # end
   #
-  # get '/signup' => 'users#new'
-  # post '/users' => 'users#create'
-  #
-  # get '/signin' => 'sessions#new'
-  # post '/signin' => 'sessions#create'
-  # get '/signout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
